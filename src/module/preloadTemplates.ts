60cd31c33138457ca5180991a56019824805c40f
Grave <samuel.grave@pier.digital>
2022-03-07 19:42:42 -0300

    feat(skill-sheet): creates the skill sheet

--- src/module/preloadTemplates.ts
+++ src/module/preloadTemplates.ts
@@ -1,5 +1,9 @@
 export async function preloadTemplates(): Promise<Handlebars.TemplateDelegate[]> {
-  const templatePaths: string[] = ['systems/kalymba/templates/item/equipment-sheet.hbs'];
+  const templatePaths: string[] = [
+    'systems/kalymba/templates/attribute-select.hbs',
+    'systems/kalymba/templates/item/equipment-sheet.hbs',
+    'systems/kalymba/templates/item/skill-sheet.hbs',
+  ];
 
   return loadTemplates(templatePaths);
 }
