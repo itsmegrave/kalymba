eff97e59c5578898551ec2002cbf2f3ef8a19a4c
Grave <samuel.grave@pier.digital>
2022-03-07 19:42:42 -0300

    feat(skill-sheet): creates the skill sheet

--- src/module/config.ts
+++ src/module/config.ts
@@ -16,12 +16,43 @@ export const KALYMBA: KalymbaConfig = {
     goldGana: 'KALYMBA.CURRENCY.GOLD_GANA',
   },
   log_prefix: 'kalymba |',
+  attributes: {
+    strength: {
+      long: 'KALYMBA.ATTRIBUTES.STRENGTH.LONG',
+      short: 'KALYMBA.ATTRIBUTES.STRENGTH.SHORT',
+    },
+    agility: {
+      long: 'KALYMBA.ATTRIBUTES.AGILITY.LONG',
+      short: 'KALYMBA.ATTRIBUTES.AGILITY.SHORT',
+    },
+    stamina: {
+      long: 'KALYMBA.ATTRIBUTES.STAMINA.LONG',
+      short: 'KALYMBA.ATTRIBUTES.STAMINA.SHORT',
+    },
+    intellect: {
+      long: 'KALYMBA.ATTRIBUTES.INTELLECT.LONG',
+      short: 'KALYMBA.ATTRIBUTES.INTELLECT.SHORT',
+    },
+    ginga: {
+      long: 'KALYMBA.ATTRIBUTES.GINGA.LONG',
+      short: 'KALYMBA.ATTRIBUTES.GINGA.SHORT',
+    },
+    ori: {
+      long: 'KALYMBA.ATTRIBUTES.ORI.LONG',
+      short: 'KALYMBA.ATTRIBUTES.ORI.SHORT',
+    },
+  },
 };
 
+// {
+//   long: 'KALYMBA.ATTRIBUTES.STRENGTH.LONG';
+//   short: 'KALYMBA.ATTRIBUTES.STRENGTH.SHORT';
+// }
 export interface KalymbaConfig {
   ASCII: string;
   currencyType: CurrencyType;
   log_prefix: string;
+  attributes: Attributes;
 }
 
 export interface CurrencyType {
@@ -30,4 +61,31 @@ export interface CurrencyType {
   bronzeGana: string;
   silverGana: string;
   goldGana: string;
+}
+
+export interface Attributes {
+  strength: {
+    long: string;
+    short: string;
+  };
+  agility: {
+    long: string;
+    short: string;
+  };
+  stamina: {
+    long: string;
+    short: string;
+  };
+  intellect: {
+    long: string;
+    short: string;
+  };
+  ginga: {
+    long: string;
+    short: string;
+  };
+  ori: {
+    long: string;
+    short: string;
+  };
 }
